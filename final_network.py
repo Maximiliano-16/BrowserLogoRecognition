160953977e5458c7a59ae4384b1ad0a80d7ac9ab
Max <98755619+Maximiliano-16@users.noreply.github.com>
2024-10-11 14:13:18 +0300

    Update final_network.py

--- final_network.py
+++ final_network.py
@@ -1,8 +1,15 @@
 import random
+import sys
+import pickle
 
 # Сторонние библиотеки
 import numpy as np
 import pandas as pd
+import csv
+
+dt = np.dtype(np.float64)
+loss_arr = []
+np.seterr(divide='ignore')
 
 
 class Network(object):
@@ -14,14 +21,22 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
-        self.t = [0]*sizes[-1]
-
+        self.t = [0] * sizes[-1]
 
         ######## new
         self.biases = [np.random.randn(1, y) for y in sizes[1:]]
-        self.weights = [np.random.randn(x, y)
+        self.weights = [np.random.randn(x, y, )
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
+        # print('INIT PARAMS')
+        print(f'self.num_layers = {self.num_layers},\n'
+              f'self.sizes = {self.sizes}\n'
+              f'self.biases = {self.biases[:1]}\n'
+              f'self.weights = {self.weights[:1]}\n'
+              f'type self.biases = {type(self.biases[0])}\n'
+              f'type self.weights = {type(self.weights[0])}\n'
+
+              f'---------------------')
 
     def feedforward(self, a):
         """Возвращает выходные данные сети, когда ``a`` - входные данные."""
@@ -30,28 +45,26 @@ class Network(object):
             # a = sigmoid(np.dot(w, a)+b)
         return a
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
-            test_data=None):
+    def SGD(self, training_data, epochs, eta, test_data=None):
         """Обучаем сеть при помощи мини-пакетов и стохастического градиентного спуска. training_data – список кортежей "(x, y)", обозначающих обучающие входные данные и желаемые выходные. Остальные обязательные параметры говорят сами за себя. Если test_data задан, тогда сеть будет оцениваться относительно проверочных данных после каждой эпохи, и будет выводиться текущий прогресс. Это полезно для отслеживания прогресса, однако существенно замедляет работу. """
-
 
         if test_data: n_test = len(test_data)
         n = len(training_data)
         for j in range(epochs):
+            random.shuffle(training_data)
             for i in range(n):
-
-                # random.shuffle(training_data)
-                nabla_b = [np.zeros(b.shape) for b in self.biases]
-                nabla_w = [np.zeros(w.shape) for w in self.weights]
                 x, y = training_data[i][0], training_data[i][1]
-                # print(f'x = {x}, y = {y}')
                 delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-                nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-                nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-                self.weights = [w - (eta / len(training_data[i])) * nw
-                                for w, nw in zip(self.weights, nabla_w)]
-                self.biases = [b - (eta / len(training_data[i])) * nb
-                               for b, nb in zip(self.biases, nabla_b)]
+
+                counter = 0
+                for w, dw in zip(self.weights, delta_nabla_w):
+                    self.weights[counter] = w - eta * dw
+                    counter += 1
+
+                counter = 0
+                for b, db in zip(self.biases, delta_nabla_b):
+                    self.biases[counter] = b - eta * db
+                    counter += 1
 
             if test_data:
                 print("Epoch {0}: {1} / {2}".format(
@@ -62,61 +75,59 @@ class Network(object):
 
     def backprop(self, x, y):
         """Вернуть кортеж ``(nabla_b, nabla_w)``, представляющий градиент для функции стоимости C_x.  ``nabla_b`` и ``nabla_w`` - послойные списки массивов numpy, похожие на ``self.biases`` and ``self.weights``."""
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
-
+        nabla_b = [np.zeros(b.shape, dtype=dt) for b in self.biases]
+        nabla_w = [np.zeros(w.shape, dtype=dt) for w in self.weights]
 
         # прямой проход
         activation = x
-        activations = [x] # список для послойного хранения активаций
+        activations = [x]  # список для послойного хранения активаций
         counter = 0
-        zs = [] # список для послойного хранения z-векторов
+        zs = []  # список для послойного хранения z-векторов
         activation = np.matrix(activation)
         for b, w in zip(self.biases[:-1], self.weights[:-1]):
             z = activation @ w + b
+            # print(f'z = {z}\n')
             zs.append(z)
             relu = ReLu(z)
             activation = relu
             activations.append(activation)
-            counter+=1
+            # print(f'activations = {activations}\n')
+            counter += 1
 
         # # Последний слой
         b = self.biases[-1]
         w = self.weights[-1]
         z = activation @ w + b
         zs.append(z)
-        activation = self.newsoftmax(z)
+        activation = self.softmax(z)
         activations.append(activation)
         counter += 1
 
         # обратный проход
         res_entropy = self.binary_cross_entropy(y, activations[-1])
-
-        ##########################################
-        # ДЛЯ ТРЕХ СЛОЕВ
-
         dE_dtlast = self.cost_derivative(activations[-1], y)
+        dE_dt1 = dE_dtlast
+        for i in range(2, len(activations)):
+            dE_dW2 = activations[-i].T @ dE_dtlast
+            dE_db2 = dE_dt1
+            dE_dh1 = dE_dt1 @ self.weights[-i + 1].T
+            dE_dt1 = np.array(dE_dh1, dtype=dt) * np.array(ReLu_deriv(zs[-i]),
+                                                           dtype=dt)
+            nabla_b[-i + 1] = dE_db2
+            nabla_w[-i + i] = dE_dW2
 
-        dE_dW2 = activations[-2].T @ dE_dtlast
-
-        dE_db2 = dE_dtlast
-        dE_dh1 = dE_dtlast @ self.weights[-1].T
-
-        dE_dt1 = np.array(dE_dh1) * np.array(ReLu_deriv(zs[-2]))
-        dE_dW1 = np.matrix(activations[0]).T @ dE_dt1
+        dE_dW1 = np.matrix(activations[0], dtype=dt).T @ dE_dt1
         dE_db1 = dE_dt1
-
         nabla_b[0] = dE_db1
         nabla_w[0] = dE_dW1
-        nabla_b[1] = dE_db2
-        nabla_w[1] = dE_dW2
 
+        loss_arr.append(res_entropy)
         return (nabla_b, nabla_w)
 
     def my_evaluate(self, test_data):
-        test_result = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        test_result = [(np.argmax(self.feedforward(x)), y) for (x, y) in
+                       test_data]
         print(test_result)
-
 
     def evaluate(self, test_data):
         """Вернуть количество проверочных входных данных, для которых нейросеть выдаёт правильный результат. Выходные данные сети – это номер нейрона в последнем слое с наивысшим уровнем активации."""
@@ -126,34 +137,46 @@ class Network(object):
 
     def cost_derivative(self, output_activations, y):
         """Вернуть вектор частных производных (чп C_x / чп a) для выходных активаций."""
-        return (output_activations-y)
+        return (output_activations - y)
 
     def binary_cross_entropy(self, t, p):
 
         t = np.float_(t)
         p = np.float_(p).T
-        # binary cross-entropy loss
         return -np.sum(t * np.log(p) + (1 - t) * np.log(1 - p))
 
-    def softmax(self, z):
-        """Softmax функция для последнего слоя."""
-        exp_z = np.exp(z - np.max(z))  # Для стабильности
-        return exp_z / exp_z.sum(axis=0)
 
-    def newsoftmax(self, z):
-        out = np.exp(z)
+    def min_max_scaler(self, data, new_min=-20, new_max=20):
+        # Преобразуем данные в двумерный массив, если они одномерные
+        data = np.array(data)
+        if data.ndim == 1:
+            data = data.reshape(1, -1)
+
+        # Находим минимум и максимум в каждом столбце
+        min_vals = np.min(data, axis=1, keepdims=True)
+        max_vals = np.max(data, axis=1, keepdims=True)
+
+        # Масштабируем данные
+        scaled_data = new_min + (data - min_vals) / (max_vals - min_vals) * (
+                new_max - new_min)
+        return scaled_data
+
+    def softmax(self, x):
+        scaled_data = self.min_max_scaler(x)
+        out = np.exp(scaled_data)
         return out / np.sum(out)
+
 
     def predict(self, x):
         t1 = x @ self.weights[0] + self.biases[0]
         h1 = ReLu(t1)
         t2 = h1 @ self.weights[1] + self.biases[1]
-        z = self.newsoftmax(t2)
+        z = self.softmax(t2)
         return z
 
     def calc_accuracy(self, data):
         correct = 0
-        for i in range(len(data)-1):
+        for i in range(len(data) - 1):
             x, y = data[i][0], data[i][1]
             z = self.predict(x)
             y_pred = np.argmax(z)
@@ -164,91 +187,41 @@ class Network(object):
         acc = correct / len(data)
         return acc
 
-    def save_model_csv(self, weights, biases, sizes, filename):
-        try:
-            with open(filename, 'w') as f:
-                # Сохранение размера сети
-                f.write(','.join(map(str, sizes)) + '\n')
-
-                # Сохранение весов
-                for i, w in enumerate(weights):
-                    layer_weights = np.concatenate(
-                        w).flatten()  # Преобразование в одномерный массив
-                    f.write(','.join(map(str, layer_weights)) + '\n')
-
-                # Сохранение смещений
-                for i, b in enumerate(biases):
-                    layer_biases = b.flatten()  # Преобразование в одномерный массив
-                    f.write(','.join(map(str, layer_biases)) + '\n')
-
-            print(f"Модель успешно сохранена в '{filename}'.")
-        except Exception as e:
-            print(f"Ошибка при сохранении модели: {e}")
-
-    def load_model_csv(self, filename):
-
-        # Загрузка размеров сети
-        sizes = []
-        self.biases = []
-        self.weights = []
-        with open(filename, 'r') as f:
-            sizes = list(map(int, f.readline().strip().split(',')))
-
-            layer_index = 0
-
-            # Загрузка весов
-            for _ in range(len(sizes) - 1):
-                layer_weights = np.array(list(map(float, f.readline().strip().split(','))))
-
-                # Восстанавливаем форму
-                layer_weights = layer_weights.reshape(sizes[layer_index],
-                                                      sizes[layer_index+1])
-
-                self.weights.append(layer_weights)
-                layer_index += 1
-
-            # Загрузка смещений
-            layer_index = 0
-            for _ in range(len(sizes) - 1):
-                layer_biases = np.array(list(map(float, f.readline().strip().split(','))))
-
-                # Восстанавливаем форму
-                # layer_biases = layer_biases.reshape(sizes[layer_index+1],
-                #                                     1)  # Столбец
-                layer_biases = np.array([layer_biases])
-                self.biases.append(layer_biases)
-                layer_index += 1
-
-        print(f"Модель успешно загружена из '{filename}'.")
-        self.sizes = sizes
-        self.num_layers = len(sizes)
-
 
 #### Разные функции
 def sigmoid(z):
     """Сигмоида."""
-    return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
+
 
 def ReLu(z):
     return np.maximum(z, 0)
 
+
 def ReLu_deriv(z):
     return (z >= 0).astype(float)
+
 
 def sigmoid_prime(z):
     """Производная сигмоиды."""
     sig = sigmoid(z)
     print(f'sig = {sig}\n')
     print(f'type(sig) = {type(sig)}\n')
-    print(f'(1-sig) = {(1-sig)}')
-    return sig @ (1-sig).transpose()
+    print(f'(1-sig) = {(1 - sig)}')
+    return sig @ (1 - sig).transpose()
 
-df = pd.read_csv('D:/MLUniversity/work1/Dataset/BrowserLogos/output.csv')
+
+df = pd.read_csv(
+    'D:/MLUniversity/work1/Dataset/BrowserLogos_64/final_output_64.csv')
+# df = pd.read_csv('D:/MLUniversity/work1/Dataset/BrowserLogos/output.csv')
+
+# sys.exit(0)
 
 training_data = []
 training_data2 = []
 class_label = []
 pixels = []
+test_data2 = []
 
 class_mapping = {
     'Amigo': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -261,34 +234,71 @@ class_mapping = {
     'Via': [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
     'Vivaldi': [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
     'Yandex': [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    # Добавьте другие классы, если необходимо
 }
 
 first_row = df.iloc[0]
 
 rows = df.iloc[0:2]
 
-
 # Определяем размер тестовой выборки
 test_size = int(0.2 * len(df))
 
 # Получаем случайные индексы для тестовой выборки
-test_indices = df.sample(n=test_size, random_state=42).index
+test_indices = df.sample(n=test_size, random_state=129).index
 
 # Разделяем выборки
 train_df = df.drop(test_indices)
 test_df = df.loc[test_indices]
-train_df = train_df.sample(frac=1, random_state=42)
+train_df = train_df.sample(frac=1, random_state=629)
 
-for i in range(len(df)):
-    pixels = list(map(int, df['pixels'][0].split(',')))
-    class_label = class_mapping[df['class'][1]]
+print(df.info(memory_usage='deep'))
+
+
+for j in range(20):
+    my_data = [random.randint(0, 1) for i in range(10)]
+    class_label = [0, 1, 0]
+    training_data.append((my_data, class_label))
+
+for i in range(1, len(train_df)):
+    pixels = list(map(int, train_df.iloc[i]['pixels'].split(',')))
+    class_label = class_mapping[train_df.iloc[i]['class']]
+    # class_label = [0, 1, 0]
     training_data2.append((pixels, class_label))
 
+for i in range(1, len(test_df)):
+    pixels = list(map(int, test_df.iloc[i]['pixels'].split(',')))
+    class_label = class_mapping[test_df.iloc[i]['class']]
+    # class_label = [0, 1, 0]
+    test_data2.append((pixels, class_label))
 
-net = Network([16384, 1024, 10])
-net.SGD(training_data2, 3, 1, 0.1)
-acc = net.calc_accuracy(data=training_data2)
+net = Network([4096, 128, 10])
 
-# acc = self.calc_accuracy(data=training_data)
+net.SGD(training_data2, 30, 0.01)
+
+# Сохранение весов модели
+
+# with open('model_params_weight.pkl', 'wb') as f:
+#     pickle.dump(net.weights, f)
+#
+# with open('model_params_biases.pkl', 'wb') as f:
+#     pickle.dump(net.biases, f)
+
+# Загрузка весов модели
+
+# with open('model_params_weight.pkl', 'rb') as f:
+#     net.weights = pickle.load(f)
+#
+# with open('model_params_biases.pkl', 'rb') as f:
+#     net.biases = pickle.load(f)
+
+
+acc = net.calc_accuracy(data=test_data2)
+
 print(f'Accuracy = {acc}')
+
+import matplotlib.pyplot as plt
+
+plt.plot(loss_arr)
+plt.show()
 
